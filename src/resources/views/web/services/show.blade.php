494d67e143407a4de5ee372246fe8c750630dc92
Labrock <vladimirpeshkov5@yandex.ru>
2025-08-15 12:46:15 +0300

    adaptive

--- src/resources/views/web/services/show.blade.php
+++ src/resources/views/web/services/show.blade.php
@@ -2,9 +2,9 @@
     @include("sc::web.services.includes.show-metas")
     @include("sc::web.services.includes.show-breadcrumbs")
 
-    <div class="container">
+    <div class="container mb-indent">
         <div class="row">
-            <div class="col w-full md:w-2/3">
+            <div class="col w-full lg:w-2/3">
                 <x-tt::h1 class="mb-indent">{{ $service->title }}</x-tt::h1>
                 @if ($blocks)
                     @foreach($blocks as $block)
@@ -12,13 +12,13 @@
                     @endforeach
                 @endif
             </div>
-            <div class="col w-full md:w-1/3">
+            <div class="col w-full lg:w-1/3">
                 @if (config("service-catalog.useImageOnShowPage") && $service->image)
                     <img src="{{ route('thumb-img', ['template' => 'service-show', 'filename' => $service->image->file_name]) }}"
-                         alt="" class="rounded-base mb-indent">
+                         alt="" class="rounded-base mb-indent hidden lg:block">
                 @endif
                 <div class="rounded-base bg-primary/25 px-indent py-indent-double sticky top-0">
-                    <x-tt::h3 class="mb-indent">Свяжитесь с нами</x-tt::h3>
+                    <x-tt::h3 class="mb-indent">Свяжитесь <span class="text-nowrap">с нами</span></x-tt::h3>
                     <livewire:sc-web-service-form :$service />
                 </div>
             </div>
